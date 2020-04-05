0fc1fdba87fb184ae3b2e8aa86a2edc126eb580b
sgulraiz <sgulraiz@gmail.com>
2020-04-05 16:10:46 -0400

    Added While Statement Examples

---
+++ java-basics/src/forLoopExamples.java
@@ -0,0 +1,10 @@
+public class forLoopExamples {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 1000; i++){
+            System.out.println(i);
+        }
+
+    }
+}
---
+++ java-basics/src/whileStatements.java
@@ -0,0 +1,18 @@
+public class whileStatements {
+
+    public static void main(String[] args ) {
+
+        //While statement
+        int count = 1;
+        while (count < 10) {
+            System.out.println(count);
+            count++;
+        }
+        // Do While Statement
+        count = 1;
+        do {
+            System.out.println(count);
+            count ++;
+        } while (count < 10);
+    }
+}
