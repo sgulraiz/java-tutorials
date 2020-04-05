f3952a01f723c1cb533c26b117f757ec0bdb6870
sgulraiz <sgulraiz@gmail.com>
2020-04-05 19:56:27 -0400

    Added Parse From String Examples

---
+++ java-basics/src/ParseFromString.java
@@ -0,0 +1,21 @@
+public class ParseFromString {
+
+    //This is the example where we can parse the integer or double value from string and use the values for computation
+
+    public static void main(String[] args) {
+
+        String numberAsString = "2020";
+        int number = Integer.parseInt(numberAsString);
+
+        System.out.println("numberAsString " + numberAsString);
+        System.out.println("number" + number);
+
+        numberAsString += 1;
+        number += 1;
+
+        System.out.println("numberAsString " + numberAsString);
+        System.out.println("number " + number);
+
+
+    }
+}
