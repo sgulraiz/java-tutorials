44fc599cce99a9a9f94318313c70fe8a1cddb119
sgulraiz <sgulraiz@gmail.com>
2020-04-05 15:03:06 -0400

    Added Switch Statement Examples

--- java-basics/src/SwitchStatements.java
+++ java-basics/src/SwitchStatements.java
@@ -16,6 +16,37 @@ public class SwitchStatements {
             default:
                 System.out.println("The value is " + switchValue);
         }
+        //call print day of the week method
+        printDayOfTheWeek(2);
     }
 
+    //Print day of the week example - creating a method
+
+    public static void printDayOfTheWeek(int day){
+        switch (day) {
+            case 1:
+                System.out.println("Sunday");
+                break;
+            case 2:
+                System.out.println("Monday");
+                break;
+            case 3:
+                System.out.println("Tuesday");
+                break;
+            case 4:
+                System.out.println("Wednesday");
+                break;
+            case 5:
+                System.out.println("Thursday");
+                break;
+            case 6:
+                System.out.println("Friday");
+                break;
+            case 7:
+                System.out.println("Saturday");
+                break;
+            default:
+                System.out.println("Invalid Day!!!");
+        }
+    }
 }
